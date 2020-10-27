2d1bb955106a7964a0b28218106ce12da339be94
TeslaSTech <valmik.revankar@mcvts.org>
2020-10-27 16:03:28 -0400

    Added "truncate" method to satellite.py, and started work on merchant.py

--- lib/merchant.py
+++ lib/merchant.py
@@ -1,6 +1,16 @@
 # merchant.py
+# Written by Valmik Revankar
+
+import random
+price_multiplier = [0, 0]
+
+
+def choose_merch():
+	chosen_merch = int(input("What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) Land\nChoose your field: "))
+	market_segment = random.randint(1,3)
+	if chosen_merch == 1:
+		price_multiplier = [1.50 * market_segment, 2, 12.5 * market_segment, 2]
+
 
 def main():
-	def merchandise():
-			def choose_merch():
-				chosen_merch = int(input("Choose your merchandise:\n1) Clothing"))
+	choose_merch()
--- lib/satellite.py
+++ lib/satellite.py
@@ -13,6 +13,15 @@ def clear():  # Clear screen function
         _ = system('clear')
 
 
+def truncate(f, n):
+    # Truncates/pads a float f to n decimal places without rounding
+    s = '{}'.format(f)
+    if 'e' in s or 'E' in s:
+        return '{0:.{1}f}'.format(f, n)
+    i, p, d = s.partition('.')
+    return '.'.join([i, (d+'0'*n)[:n]])
+
+
 def job_choice():  # choices
     job = ""
 
