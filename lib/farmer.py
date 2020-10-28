066f7d1d1337010663198493464a908409443c2a
d4-rk <ib.muhammad725@gmail.com>
2020-10-28 12:04:41 -0400

    Added first choice for farmer

--- lib/farmer.py
+++ lib/farmer.py
@@ -1,6 +1,19 @@
 # farmer.py
 
 def main():
-    # put the code here
-    def farm_choice():
-        print("What will you be a farmer of?\n1) ")
+    farm = None
+
+    # choose the type of farm
+    def choose_farm():
+        chosen_farm = int(input("What will your farm mostly be for?\n1) livestock \n2) crops"))
+        if chosen_farm != 1 or chosen_farm != 2:
+            print("\nPlease enter a valid farm value\n")
+            choose_farm()
+        return chosen_farm
+
+    farm = choose_farm()
+
+    if farm == 1:
+        input("\nYour farm has 4 horses, 25 cattle, 15 sheep, and 13 pigs. Today you need to go into the market and "
+              "buy some items. "
+              "\nPress ENTER to go to the market")
