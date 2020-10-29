e7ec6d0ce88e845b7e39eb8cfd1ba5001bdd4bdf
TeslaSTech <valmik.revankar@mcvts.org>
2020-10-29 19:00:51 -0400

    Added append to the saves_proc.py library

--- lib/market.py
+++ lib/market.py
@@ -24,6 +24,7 @@ class Market:
                        tablefmt="grid"))
 
 
+
 def main():
     Market.welcome()
     Market.menu_display()
--- lib/saves_proc.py
+++ lib/saves_proc.py
@@ -7,3 +7,10 @@ def write_dict(text, dict_in):
 def read_dict(text):
     with open(text, "r") as f:
         return eval(f.read())
+
+
+def append_dict(text, values_to_add):
+    dict_in = read_dict(text)
+    dict_in.update(values_to_add)
+    with open(text, "w") as f:
+        f.write(dict_in)
