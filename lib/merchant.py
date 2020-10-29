4c2cfb824ef492cf11aa7d56a614a9b56b9d56b3
TeslaSTech <valmik.revankar@mcvts.org>
2020-10-29 16:00:21 -0400

    Continued work on merchant.py, and made it class-based!

--- lib/merchant.py
+++ lib/merchant.py
@@ -13,27 +13,26 @@ class Merchant:
 		elif chosen_merch == 2:
 			price_multiplier = [50 * self.market_segment, 125 * self.market_segment]
 		elif chosen_merch == 3:
-			price_multiplier = [ * self.market_segment, 125 * self.market_segment]
+			price_multiplier = [5000 * self.market_segment, 12500 * self.market_segment]
 		elif chosen_merch < 3:
 			print("Apparently you've managed to break this program.")
 
-<<<<<<< HEAD
 	def choose_merch(self=None):
-		chosen_merch = int(input("What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) "
-								 "Land\nChoose your field: "))
+		chosen_merch = int(input("What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) \
+								Land\nChoose your field: "))
+
+		while chosen_merch < 1 or chosen_merch > 3:
+			chosen_merch = int(input("That's an invalid answer choice. What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) \
+											Land\nChoose your field: "))
+
 		if chosen_merch == 1:
 			price_multiplier = [1.50 * self.market_segment, 12.5 * self.market_segment]
 		elif chosen_merch == 2:
 			price_multiplier = [50 * self.market_segment, 125 * self.market_segment]
 		elif chosen_merch == 3:
-			price_multiplier = [ * self.market_segment, 125 * self.market_segment]
-=======
-def choose_merch():
-	chosen_merch = int(input("What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) Land\nChoose your field: "))
-	market_segment = random.randint(1,3)
-	if chosen_merch == 1:
-		price_multiplier = [1.50 * market_segment, 2, 12.5 * market_segment, 2]
->>>>>>> origin/master
+			price_multiplier = [5000 * self.market_segment, 12500 * self.market_segment]
+		elif chosen_merch < 3:
+			print("Apparently you've managed to break this program.")
 
 
 def main():
