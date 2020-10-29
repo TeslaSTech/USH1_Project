4e6687c5d7a2544d53a49273fc1296721f2569b5
d4-rk <ib.muhammad725@gmail.com>
2020-10-29 15:36:51 -0400

    added market file and finished first option for farmer

--- lib/farmer.py
+++ lib/farmer.py
@@ -1,12 +1,16 @@
 # farmer.py
+# Written by Muhammad Ibrahim
+
+import lib.market as market
 
 def main():
     farm = None
 
     # choose the type of farm
     def choose_farm():
-        chosen_farm = int(input("What will your farm mostly be for?\n1) livestock \n2) crops\nEnter your choice (1 or 2)"))
-        if chosen_farm != 1 or chosen_farm != 2:
+        chosen_farm = int(input("What will your farm mostly be for?\n1) livestock \n2) crops\nEnter your choice (1 or "
+                                "2) "))
+        if chosen_farm != 1 and chosen_farm != 2:
             print("\nPlease enter a valid farm value\n")
             choose_farm()
         return chosen_farm
@@ -15,5 +19,7 @@ def main():
 
     if farm == 1:
         input("\nYour farm has 4 horses, 25 cattle, 15 sheep, and 13 pigs. Today you need to go into the market and "
-              "buy some items. "
-              "\nPress ENTER to go to the market")
+              "buy some items.\nPress ENTER to go to the Market")
+    elif farm == 2:
+        input("\nYou have a few cattle and sheep, but most of your farm is for beans, corn, pumpkins, squash, "
+              "and rye. Today you need to go into the market and buy some items\nPress ENTER to go to the Market")
--- lib/merchant.py
+++ lib/merchant.py
@@ -6,7 +6,8 @@ price_multiplier = [0, 0]
 
 
 def choose_merch():
-	chosen_merch = int(input("What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) Land\nChoose your field: "))
+	chosen_merch = int(input("What will you be selling? Choose from the following:\n1) Clothing\n2) Firearms\n3) "
+							 "Land\nChoose your field: "))
 	market_segment = random.randint(1,3)
 	if chosen_merch == 1:
 		price_multiplier = [1.50 * market_segment, 2, 12.5 * market_segment, 2]
