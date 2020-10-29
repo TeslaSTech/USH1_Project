1ffba425e3945dd8a3e0e2b42e1b93ef99f03e8b
d4-rk <ib.muhammad725@gmail.com>
2020-10-29 15:39:33 -0400

    added market file and finished first option for farmer

--- lib/farmer.py
+++ lib/farmer.py
@@ -17,9 +17,12 @@ def main():
 
     farm = choose_farm()
 
+    # deal with choices and force user to go to the market
     if farm == 1:
         input("\nYour farm has 4 horses, 25 cattle, 15 sheep, and 13 pigs. Today you need to go into the market and "
               "buy some items.\nPress ENTER to go to the Market")
+        market.main()
     elif farm == 2:
         input("\nYou have a few cattle and sheep, but most of your farm is for beans, corn, pumpkins, squash, "
               "and rye. Today you need to go into the market and buy some items\nPress ENTER to go to the Market")
+        market.main()
